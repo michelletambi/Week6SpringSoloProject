1c1d796bc7ede7b8b89fab04ae7eacdf6a036a8c
Michelle Tambi <michelletambi22@gmail.com>
2022-05-19 11:58:49 +0100

    populated Athlete controller class

--- SportsandAthletes/src/main/java/com/example/SportsandAthletes/controller/AthleteController.java
+++ SportsandAthletes/src/main/java/com/example/SportsandAthletes/controller/AthleteController.java
@@ -1,7 +1,27 @@
 package com.example.SportsandAthletes.controller;
 
+import com.example.SportsandAthletes.model.Athlete;
+import com.example.SportsandAthletes.repository.AthleteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
+@RestController
 public class AthleteController {
+
+    @Autowired
+    AthleteRepository athleteRepository;
+
+    @GetMapping("/athletes")
+    public ResponseEntity<List<Athlete>> getAllAthlete(){
+        List<Athlete> athletes = athleteRepository.findAll();
+        athletes = athleteRepository.findAll();
+        return ResponseEntity
+                .ok()
+                .body(athletes);
+    }
+
 }
--- SportsandAthletes/src/main/java/com/example/SportsandAthletes/controller/SportController.java
+++ SportsandAthletes/src/main/java/com/example/SportsandAthletes/controller/SportController.java
@@ -1,4 +1,9 @@
 package com.example.SportsandAthletes.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SportController {
+
+
 }
