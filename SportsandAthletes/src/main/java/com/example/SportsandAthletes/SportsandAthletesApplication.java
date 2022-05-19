c5c08914ce6f9fd3dd6ef775c0b31f1a9e970e9b
Michelle Tambi <michelletambi22@gmail.com>
2022-05-19 10:20:27 +0100

    Initialised sportsAndAthletesproject

---
+++ SportsandAthletes/src/main/java/com/example/SportsandAthletes/SportsandAthletesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SportsandAthletes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsandAthletesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsandAthletesApplication.class, args);
+	}
+
+}
---
+++ SportsandAthletes/src/test/java/com/example/SportsandAthletes/SportsandAthletesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SportsandAthletes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsandAthletesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
