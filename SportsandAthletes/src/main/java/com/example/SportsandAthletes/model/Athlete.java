732f620fb82dd8ada8dcddcf0e200bfe21528fad
Michelle Tambi <michelletambi22@gmail.com>
2022-05-19 10:37:17 +0100

    added @entity annotation to both model classes and aut-generation to id in both model classes

--- SportsandAthletes/src/main/java/com/example/SportsandAthletes/model/Athlete.java
+++ SportsandAthletes/src/main/java/com/example/SportsandAthletes/model/Athlete.java
@@ -1,6 +1,14 @@
 package com.example.SportsandAthletes.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Athlete {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
--- SportsandAthletes/src/main/java/com/example/SportsandAthletes/model/Sport.java
+++ SportsandAthletes/src/main/java/com/example/SportsandAthletes/model/Sport.java
@@ -1,8 +1,14 @@
 package com.example.SportsandAthletes.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
-
+@Entity
 public class Sport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String location;
