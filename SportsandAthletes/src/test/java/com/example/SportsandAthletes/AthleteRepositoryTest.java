b856bda5c6eadcd77fbdea4b05b304d9a8d54199
Michelle Tambi <michelletambi22@gmail.com>
2022-05-30 16:22:22 +0100

    Added tests for two derived queries

--- SportsandAthletes/src/test/java/com/example/SportsandAthletes/AthleteRepositoryTest.java
+++ SportsandAthletes/src/test/java/com/example/SportsandAthletes/AthleteRepositoryTest.java
@@ -1,4 +1,41 @@
 package com.example.SportsandAthletes;
 
+import com.example.SportsandAthletes.model.Athlete;
+import com.example.SportsandAthletes.repository.AthleteRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@SpringBootTest
 public class AthleteRepositoryTest {
+
+    @Autowired
+    private AthleteRepository athleteRepository;
+
+    @Test
+    void contextLoads(){
+    }
+
+    @Test
+    public void canFindByCountry(){
+//        given athleteRepository
+//        when
+        List<Athlete> found = athleteRepository.findByCountry("Sweden");
+//        then
+        assertEquals(found.size(), 2);
+    }
+
+    @Test
+    public void canFindAllOfGender(){
+//        given athleteRepository
+//        when
+        List<Athlete> found = athleteRepository.findAllOfGender("Female");
+//        then
+        assertEquals(found.size(), 11);
+
+    }
+
 }
